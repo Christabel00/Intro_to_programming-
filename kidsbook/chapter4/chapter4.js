0476fb91a23a61554f4d50299600c571f56d7907
Christabel00 <134796750+Christabel00@users.noreply.github.com>
2023-12-02 00:13:48 +0200

    Add files via upload

---
+++ kidsbook/chapter4/chapter4.js
@@ -0,0 +1,160 @@
+// let cat = {
+//    legs :3,
+//    name : "Harmony",
+//    color : "Tortoiseshell"
+// };
+
+
+//keys without quotes
+// let cat = {
+// legs :3 ,
+// name  :"hamorny",
+// color : "tortoiseShell"
+// };
+// here legs ,name and color are keys of the object cat
+// // console.log(cat);
+
+
+//displaying the keys of an object using dot notion
+// console.log(cat.name);
+// console.log(cat.color);
+
+//using square brackets to display keys of an object
+// console.log(cat["name"]);
+
+
+//object.keys method 
+// let people ={name : "Thando", age :4 , haircolor :"blue" };
+// let cats = {name : "doris" , age : 6 , color: "white"};
+
+//To acces keys of a cat 
+//  console.log(Object.keys(people));
+//  console.log(Object.keys(cats));
+
+//Adding values to objects 
+// let people = {};
+// people["name"] = "Bella";
+//  people ["age"]  = 5;
+//  people ["surname"] = "Davids";
+// console.log(people);
+//  Adding keys with dot notion
+//   let people ={};
+//   people.age= 6;
+//   people.name = "Bella"
+//   people.height =57;
+//   people.surname="Ruvimbo";
+// console.log(people);
+
+ //people.name ="Bella";
+ //people["name"] ="Bella"
+
+ //people.haircolor="blue";
+ //people["haircolor"] ="blue";
+
+ 
+
+ //Combining arrays and objects.
+// let person =[ 
+//    {name: "Ruvimbo" , job :"software engineer"  },
+//    {name : "Alicia"   , job : "Technician"      },
+//    {name : "Thabi" , job: "dancer"              }
+//  ];
+
+ //to get people info
+// //  console.log(person[0]);
+
+//  //to get only the name of the 
+// // console.log(person [1] ["job"]);
+
+//  //using the dot notion
+// // console.log(person[2].job);
+
+// //AN ARRAY OF FRIENDS
+
+// // let anna = { name: "Anna", age: 11, luckyNumbers: [2, 4, 8, 16] };
+// // let dave = { name: "Dave", age: 5, luckyNumbers: [3, 9, 40] };
+// // let kate = { name: "Kate", age: 9, luckyNumbers: [1, 2, 3] };
+// // let friends =[anna , dave ,kate];
+
+// // console.log(friends[1]);
+// // console.log(friends[1].name);
+// // console.log(friends[2].luckyNumbers[2]);
+
+// //Owed money example
+
+// // let owedMoney ={};
+// // owedMoney["Jimmy"] =5;
+// // owedMoney["Mercy"] =12;
+
+// //Showing all the owed  money
+// // console.log(owedMoney);
+
+// //showing how much jimmy owes
+// // console.log(owedMoney["Jimmy"]);
+// // console.log(owedMoney["Jimmy"] +=4);
+
+// let movies = {
+//     "Finding Nemo": {
+//     releaseDate: 2003,
+//     duration: 100,
+//     actors: ["Albert Brooks", "Ellen DeGeneres", "Alexander Gould"],
+//     format: "DVD"
+//     },
+
+//     "Star Wars: Episode VI - Return of the Jedi": {
+//     releaseDate: 1983,
+//     duration: 134,
+//     actors: ["Mark Hamill", "Harrison Ford", "Carrie Fisher"],
+//     format: "DVD"
+//     },
+
+//     "Harry Potter and the Goblet of Fire": {
+//     releaseDate: 2005,
+//     duration: 157,
+//     actors: ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"],
+//     format: "Blu-ray"
+//     }
+
+//    };
+//    let findingNemo = movies["Finding Nemo"];
+//    console.log(findingNemo);
+//   console.log(findingNemo.actors);
+
+//    var cars = {
+//     releaseDate: 2006,
+//     duration: 117,
+//     actors: ["Owen Wilson", "Bonnie Hunt", "Paul Newman"],
+//     format: "Blu-ray"
+//    };
+//    movies["Cars"] = cars;
+// console.log(cars.format);
+   
+//    console.log(movies);
+
+// let scores = {
+// Ruvimbo {
+   
+// }
+
+
+
+// };
+
+
+//  let myCrazyObject = {
+//     "name": "A ridiculous object",
+//     "some array": [7, 9, { purpose: "confusion", number: 123 }, 3.3],
+//     "random animal": "Banana Shark"
+//    };
+//    let someArray = myCrazyObject["some array"];
+//   console.log(someArray[2][5]);
+
+
+
+
+
+
+
+
+
+
