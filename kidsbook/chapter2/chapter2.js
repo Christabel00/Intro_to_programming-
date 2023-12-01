63522e8aadb368272961975c66f36280aea56842
Christabel00 <134796750+Christabel00@users.noreply.github.com>
2023-12-02 00:12:18 +0200

    Add files via upload

---
+++ kidsbook/chapter2/chapter2.js
@@ -0,0 +1,81 @@
+// console.log(11527 + 46256);
+// console.log(587663/6753);
+// console.log(6532765*5476);
+// console.log(8/(3+1));
+
+//VARIABLES 
+// let nick ;
+// console.log("nick");
+
+// let age = 16;
+// console.log(age);
+// age = 14;
+// console.log(age);
+
+// let numberOfSiblings = 1+3;
+// let numberOfCandles = 8;
+//  console.log(numberOfCandles/numberOfSiblings);
+
+ //INCREMENTING AND DECREMENTING (pre increment & decrement)
+// let highFives =6;
+// ++highFives;
+// console.log(highFives);
+// --highFives;
+// console.log(highFives);
+
+//post increment & decrement
+// let highFives = 0;
+// console.log(highFives++)
+// console.log(highFives);
+ 
+//Plus equal & minus equal
+// let x=10;
+// //x= x+5;
+// x+=5;
+// console.log(x);
+
+// let number = 20;
+// number +=5;
+// console.log(number);
+
+// let score =17;
+// score -=6;
+// console.log(score);
+
+// let balloons = 100;
+// balloons *= 2;
+// console.log(balloons);
+
+// let balloon = 100;
+// balloon /= 4;
+// console.log(balloon);
+
+// //STRINGS
+
+// Draw as many cats as you want!
+// var drawCats = function (howManyTimes) {
+//     for (var i = 0; i < howManyTimes; i++) {
+//     console.log(i + " =^.^=");
+//     }
+//    };
+//    drawCats(10); // You can put any number here instead of 10
+//  let myName = "Ruvimbo  ";
+// let mySurname = "Dube";
+// console.log(myName+mySurname);
+//let me = "ruvimbo mupedziswa is a girl";
+//console.log("ruvimbo mupedziswa is a girl".toUpperCase());
+
+
+// let isWeekend = true;
+// let needToShowerToday = !isWeekend;
+// console.log(needToShowerToday);
+// var isWeekend = false;
+// var hadShower = true;
+// var hasApple = false;
+
+// var hasOrange = true;
+// var shouldGoToSchool = !isWeekend && hadShower && (hasApple || hasOrange);
+// console.log(shouldGoToSchool);
+let height = 65;
+let heightRestriction = 60;
+console.log(height === heightRestriction);
