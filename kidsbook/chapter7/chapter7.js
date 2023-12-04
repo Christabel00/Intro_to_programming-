6df4ca6a4f5fbb09b9689965f5e8bfae195e54d9
Christabel00 <134796750+Christabel00@users.noreply.github.com>
2023-12-04 07:13:15 +0200

    Add files via upload

---
+++ kidsbook/chapter7/chapter7.js
@@ -0,0 +1,43 @@
+// let name = prompt("What's your name?");
+// console.log("Hello " + name);
+
+//Using the confirm function to ask a question
+
+// let likeCats = confirm("Do you like cats?")
+// if(likeCats){
+//     console.log("Hi cat lover");
+// } 
+// else {
+//     console.log("Why do you hate them though?");
+// }
+
+//USING ALERT TO GIVE A PLAYER INFORMATION
+//alert("You are awesome!!");
+
+//CODING THE GAME
+
+// let words =[
+// "javascript" ,
+// "monkey"   ,
+// "amazing"  ,
+// "panacake"
+
+// ];
+// let word =[Math.floor(Math.random()*words.length)];
+
+// //CREATING THE ANSWER ARRAY
+// let answerArray =[];
+// for(let i=0; i>word.length; i++){
+//     answerArray[i] = "_";
+// }
+// let remainingLetters = word.length;
+// //HANDLING THE PLAYERS INPUT
+// let guess = prompt("Guess the letter or click cancel to stop");
+// if (guess=== null) {
+//     break;
+// } else if (guess.length !==1)
+// {
+//    alert ("Please enter a letter") ;
+
+// } 
+
